d364dc4f8d9fe63d8134f046b62d16aa719a7b9d
Sundram Kumar <sundramroy1208@gmail.com>
2025-01-07 22:40:15 +0530

    Weathther and hourly weather card added

--- lib/weather_home_page.dart
+++ lib/weather_home_page.dart
@@ -1,4 +1,4 @@
-
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -8,7 +8,7 @@ class WeatherHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar:  AppBar(
         title: const Text(
           "Weather App",
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -17,7 +17,7 @@ class WeatherHomePage extends StatelessWidget {
         actions: [
           IconButton.outlined(
             onPressed: () {
-              print("Refresh");
+              // print("Refresh");
             },
             icon: const Icon(
               Icons.refresh_rounded,
@@ -25,7 +25,123 @@ class WeatherHomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Text("udd gayee"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 200,
+              child:Card(
+                color: Colors.white10,
+                elevation: 10,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(25)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(20),
+                      // filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                           Text(
+                            "300.12°F",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          Icon(
+                            Icons.cloud,
+                            size: 30,
+                          ),
+                          Text(
+                            "Rain",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: double.infinity,
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: const Text(
+                "Weather Forecast",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+            //Adding the hourlyWeatherCard here
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: const Row(
+                children: [
+                  WeatherForecastCard(time: "10:00",icon:  Icons.cloud,temperature:  "300.12°F"),
+                  WeatherForecastCard(time: "12:00",icon:  Icons.cloud_done,temperature:  "300.12°F"),
+                  WeatherForecastCard(time: "14:00",icon:  Icons.cloud,temperature:  "300.12°F"),
+                  WeatherForecastCard(time: "14:00",icon:  Icons.cloud,temperature:  "300.12°F"),
+                  WeatherForecastCard(time: "14:00",icon:  Icons.cloud,temperature:  "300.12°F"),
+                ],
+              ),
+            ),
+            const SizedBox(
+              width: double.infinity,
+              height: 20,
+            ),
+            Placeholder(
+              fallbackHeight: 200,
+              fallbackWidth: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherForecastCard extends StatelessWidget {
+  final String time;
+  final IconData icon;
+  final String temperature;
+  const WeatherForecastCard({
+    super.key,
+    required this.time,
+    required this.icon,
+    required this.temperature,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 115,
+      height: 115,
+      child: Card(
+        color: Colors.white10,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 5,
+          children: [Text(time), Icon(icon), Text(temperature)],
+        ),
+      ),
     );
   }
 }
